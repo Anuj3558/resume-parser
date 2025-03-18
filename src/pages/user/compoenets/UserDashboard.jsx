0b65ee330cb52e788c2e0717b64f35c4b8cd0007
Anuj3558 <anujloharkar3557@gmail.com>
2025-03-18 19:02:20 +0530

    maanul deploy kar

--- src/pages/user/compoenets/UserDashboard.jsx
+++ src/pages/user/compoenets/UserDashboard.jsx
@@ -64,7 +64,7 @@ const UserDashboard = () => {
 			{/* Stats Cards */}
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
 				{stats.map((stat, index) => (
-					<StatsCard key={index} stat={stats} />
+					<StatsCard key={index} stat={stat} />
 				))}
 			</div>
 
